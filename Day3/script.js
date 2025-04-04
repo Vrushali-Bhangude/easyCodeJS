3885ebd962eb77389f486ff0a32650e34ff528c6
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-04-05 00:10:05 +0530

    Daata type in js

--- Day3/script.js
+++ Day3/script.js
@@ -1,3 +1,55 @@
-alert("Hey , Start your JS Journery......");
 
-console.log("Welcome JS");
+// Number 
+
+let PI = 3.14
+let pinCode = 414501 
+let mobileNumber = 811111111 
+let roll_Number = 48
+let age  = 20
+let radius = 7 
+let OTP = 1234
+
+//String 
+
+let email_id = "vrushali@gmail.com"
+let college_name = "NACSCS"
+let address = "Shevgaon "
+let city_name = "rajdhan"
+
+
+// boolean 
+// true || false
+
+let LOGINED = true
+let USER_HAVE_ACCESS = false
+let BAN = false
+
+
+//null 
+let a = null 
+
+// undefined
+
+let b 
+
+
+
+//Non-primitive 
+
+// {}  [] () 
+
+// let obj = {}
+
+// let arr =[]
+
+// let sum(){}
+
+// test 
+
+let greet = ["hello","good","morning"]
+
+function sum(params) {
+    
+}
+
+
