fdddb0ca7e4959461a888e69e139e377deddebb4
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-04-06 12:18:14 +0530

    learn about console

---
+++ Day4/script.js
@@ -0,0 +1,11 @@
+let simple_msg = "hey js"
+console.log(simple_msg);
+
+let info_msg = "today we learn about JS"
+console.info(info_msg);
+
+let warn_msg  = "this site not secure"
+console.warn(warn_msg);
+
+let err_msg = "please! check youe internet connection"
+console.error(err_msg);
