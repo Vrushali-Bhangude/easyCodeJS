5d511dc9db72991db4b30458b69671319b88e433
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-04-10 11:52:18 +0530

    learn about assignment and comparison operator

---
+++ Day5/script.js
@@ -0,0 +1,63 @@
+let name = "easyCode"
+
+//console.log(name)
+ 
+let a = 10
+
+a += 3
+
+//console.log(a)
+
+let b = 20 
+
+b -= 10
+
+//console.log(b)
+
+let c = 4
+
+c *= c
+
+//console.log(c)
+
+let d = 12
+
+d /= 2
+
+//console.log(d)
+
+let e = 12 
+
+e %= 2
+
+//console.log(e)
+
+
+let p = 12 
+let q = 12
+
+//console.log(p != q)
+
+
+let OTP = 1234
+
+let USER_OTP = "1234"
+
+//console.log(USER_OTP === OTP )
+
+let axy = 10 ;
+
+let byz = 20;
+
+let result =  10 < 20;
+
+//console.log(result)
+
+
+let res = 12 >= 12
+
+//console.log(res)
+
+let res1 =  12 <= 12
+
+console.log(res1)
