af088a8b9994b9a6b9dce61dd98e5cbaa0083978
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-04-27 23:08:11 +0530

    learn switch statement

---
+++ Dayy10/script.js
@@ -0,0 +1,143 @@
+
+// let marks = parseInt(prompt("enter your marks in % : "));
+
+
+// if(marks >= 90){
+//      console.log("you get grade A");
+
+// }else if(marks >= 80){
+//      console.log("you get gread B");
+
+// }else if(marks >= 70){
+//      console.log("you get grade C");
+
+// }else{
+//      console.log("you get grade D");
+// }
+
+
+// let ROLE = "admin";
+
+// if (ROLE === "admin") {
+
+//     console.log("you are admin")
+
+// } else if (ROLE == "user") {
+//     console.log("you are user")
+// } else {
+//     console.log("please login to get your role")
+// }
+
+
+
+// let day = "sunday"
+
+// switch (day) {
+
+//     case "monday":
+//         console.log("today is monday");
+//          break;
+
+//     case "tuesday":
+
+//         console.log("today is tuesday");
+//          break;
+
+//     case "wednesday":
+
+//         console.log("today is wednesday");
+//         break;
+
+//     case "thursday":
+
+//         console.log("today is thursday");
+//         break;
+
+//     case "friday":
+
+//         console.log("today is friday");
+//         break;
+
+//     default : 
+//       console.log("i dont have these day ")
+
+// }
+
+
+
+// let color = "pink"
+
+// switch(color){
+
+//     case "red" :
+//          console.log("color is red");
+//          break;
+
+//     case "green" :
+//         console.log("color is green");
+//         break;
+
+//     case "orange" : 
+//         console.log("color is orange");
+//         break;
+
+//     case "blue" : 
+//         console.log("color is blue");
+//         break;
+
+//     default : 
+//       console.log("i dont have these color")
+
+// }
+
+
+// let num1 = 10
+// let num2 = 30
+
+// let operation = "a"
+
+// switch(operation){
+//     case "+" :
+//         console.log(num1 + num2);
+//         break;
+
+//     case "-":
+//         console.log(num1 - num2);
+//         break;
+
+//     case "*":
+//         console.log(num1 * num2);
+//         break;
+
+//     case "/" :
+//         console.log(num1 / num2);
+//         break;
+
+//     case "%" :
+//         console.log(num1 % num2);
+//         break;
+
+//     default:
+//          console.error(operation + " is not supported")
+// }
+
+
+
+// function checkPelindrome(str) {
+
+//     if(typeof str != "string"){
+
+//          return console.log("please enter string")
+//     }
+ 
+
+//     let splitString = str.split("").reverse().join("")
+
+//     if (str === splitString) {
+//         console.log("string is pelindrome")
+//     } else {
+//         console.log("string is not pelindrome")
+//     }
+// }
+
+// checkPelindrome("n");
