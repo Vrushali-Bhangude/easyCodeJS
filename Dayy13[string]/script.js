1b0ee16aa91af58d4821f4df5320a91f2dc3f92e
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-05-18 13:48:25 +0530

    larn abiut string

---
+++ Dayy13[string]/script.js
@@ -0,0 +1,42 @@
+
+
+let name = "vrushali"
+
+let combinedData = "vrushali" + " " + "pune" + " " + 22
+
+console.log(combinedData)
+
+name.toLowerCase()
+console.log(name.toLowerCase())
+
+name.toUpperCase()
+console.log(name.toUpperCase())
+
+
+let quote =  "we Love Tto transform ideas into code" 
+
+console.log(quote)
+
+console.log(quote.length)
+
+console.log(quote.indexOf("Tto"))
+
+
+console.log(quote.slice(0, 10))
+
+console.log(quote.slice(0, 10).toUpperCase())
+
+console.log(quote.slice(0, 10).toLowerCase())
+
+console.log(quote.slice(0, 10).toUpperCase().length)
+
+console.log(quote.slice(0, 10).toLowerCase().length)
+
+console.log(quote.split(","))
+
+
+console.log(name.split(",")[1])
+console.log(name.split(",")[0])
+console.log(name.split(",")[2])
+console.log(name.split(",")[3])
+
