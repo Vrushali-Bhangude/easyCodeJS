632d714ce47da3f3a0f378242505dcc46dbc8d68
Vrushali-Bhangude <vrushalibhangude2@gmail.com>
2025-05-18 13:33:27 +0530

    learn object properties

---
+++ Dayy13[object properties]/script.js
@@ -0,0 +1,46 @@
+
+let whatsUserData = {
+     
+     name : "vrusuuu",
+
+     profile_picture : "myProfile.jpg",
+
+}
+
+
+
+  whatsUserData.name = "vrushuu"
+
+console.log(whatsUserData)
+
+
+
+let PhoneInfo = {
+     name :"iphone",
+     prise:65000,
+     color:"black",
+}
+
+
+PhoneInfo.isHaveCharger = false
+
+delete PhoneInfo.prise
+
+let ObjValues = Object.values(PhoneInfo)
+
+
+console.log(ObjValues)
+
+
+let Doubt = {
+     doubt : "how i insert new key in object"
+}
+
+Doubt.isDoubtSolve =  true
+
+Doubt.answer = "i write directly Objname.newKey = value"
+
+Doubt.date = "12/12/1024"
+
+
+console.log(Doubt)
